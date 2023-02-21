53a644528bb1375afa015d82a51d7d6d456d2151
orphygts2 <jm.millet@orange.fr>
2023-02-21 10:12:16 +0100

    maj21022023

--- choco/regressi/tools/chocolateyinstall.ps1
+++ choco/regressi/tools/chocolateyinstall.ps1
@@ -1,18 +1,21 @@
-
+﻿
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://jean-michel-millet.pagesperso-orange.fr/zip/regressi-mpeg-setup.msi'
+$url        = 'https://jean-michel-millet.pagesperso-orange.fr/regressi-choco.msi'
+
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'MSI'
   url           = $url
+ 
 
   softwareName  = 'Regressi*'
 
-  checksum      = '9D52F3E5BD0D75EB19D19CA12E1CF7E6FA93B323036D55BAD07C491AAAEF37EC'
+  checksum      = '05F8F43BF57CA84E687CEA4E9A786CAB499FDBD33AC7D3AF633D5E9F7F7F4A04'
   checksumType  = 'sha256'
+ 
 
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes= @(0, 3010, 1641)
